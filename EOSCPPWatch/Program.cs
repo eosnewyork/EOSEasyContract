78f0d8e408333aa64b23996ebf52e705366765b0
Warrick FitzGerald <warrick@eosnewyork.io>
2018-10-06 11:40:11 -0400

    Add project files.

---
+++ EOSCPPWatch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EOSCPPWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
